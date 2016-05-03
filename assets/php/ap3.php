8962c664699c50623d8d77297a3dec1cf34e4913
sengzhaotoo <stoo@hawk.iit.edu>
2016-05-02 23:06:32 -0500

    Update ap3.php

--- assets/php/ap3.php
+++ assets/php/ap3.php
@@ -18,20 +18,14 @@
         if (($row = oci_fetch_assoc($m_id)) != false){
             // Assumming that site wide moderators are 'root' users
             // Check if users have root access level
-            if ($row['ACCESS_LEVEL'] != "root"){
+            if ($row['ACCESS_LEVEL'] != "root" || $row['ACCESS_LEVEL'] != "Professor" || $row['ACCESS_LEVEL'] != "Assistant Professor"){
                 $message =  "Access level is " .  $row['ACCESS_LEVEL'] . ". You are not granted any privileges to assign moderators.";
 
                 // Prints out alert message    
-                echo "<script type='text/javascript'>alert('$message');</script>";
-                
+                echo "<script type='text/javascript'>alert('$message');</script>";                
             } else {
-                // Check for that person if he is TA 
-                // Assign the selected person to be moderator 
-                // The selected person has to be a TA/instructor 
-                /*$ta_check = oci_parse($conn, 'SELECT * FROM TA WHERE person_id = '$s_id' AND course_id = '$c_id';');*/
-                echo "Access level is " .  $row['ACCESS_LEVEL'] . ". You are an admin!";
-                
                 // Check for TA status 
+                // If he/she is a TA, then they are eligible to be assigned as a moderator
                 $p_id = oci_parse($conn, 'SELECT person_id from TA WHERE person_id = :p_id AND course_id = :course_id');
                 
                 oci_bind_by_name($p_id, ":p_id", $_POST["p_id"]); 
@@ -48,30 +42,19 @@
                         // Group ID gets its unique value 
                         $group_id = $row_g['GROUP_ID'];
                         
-                        // Update 
-                        /*$ins = oci_parse($conn, 'INSERT ALL INTO GROUPS (group_id, moderator2) VALUES (:g_id, :p_id) SELECT * FROM DUAL');*/
-                        
-                        // Query if the moderators fields are empty
-                        $q = oci_parse($conn, 'SELECT moderator1, moderator2 FROM GROUPS WHERE group_id = :g_id');
+                        // Query if the moderators are found for a course 
+                        $q = oci_parse($conn, 'SELECT p_id FROM MODERATORS WHERE group_id = :g_id');
                         
                         oci_bind_by_name($q, ":g_id", $group_id);
                         oci_execute($q, OCI_DEFAULT); 
                         
                         $row=(oci_fetch_assoc($q));
-                        if ($row['MODERATOR1'] == NULL ){
-                            $ins = oci_parse($conn, 'UPDATE GROUPS SET moderator1 = :p_id WHERE group_id = :g_id');
+                        if (!($row['P_ID'])){
+                            $ins = oci_parse($conn, 'UPDATE MODERATORS SET p_id = :p_id WHERE group_id = :g_id');
 
                             oci_bind_by_name($ins, ":p_id", $_POST["p_id"]);
                             oci_bind_by_name($ins, ":g_id", $group_id);
                             oci_execute($ins);    
-                        } else if ($row['MODERATOR2'] == NULL){
-                            $ins = oci_parse($conn, 'UPDATE GROUPS SET moderator2 = :p_id WHERE group_id = :g_id');
-
-                            oci_bind_by_name($ins, ":p_id", $_POST["p_id"]);
-                            oci_bind_by_name($ins, ":g_id", $group_id);
-                            oci_execute($ins);    
-                        } else {
-                            echo "It's impossible to assign more than 2 moderators.";
                         }
                         
                         // Commits
@@ -89,7 +72,7 @@
             }
         }
     }
-
     // Closes connection to database 
     oci_close($conn);
+    header('Location: /fyp/tables.html');
 ?>
